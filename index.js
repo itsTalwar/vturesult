861bee4318d580b5e44bee2589a3340fd29308bd
Nikhil Talwar <nikhil@boomerangcommerce.com>
2019-02-24 22:50:38 +0530

    with token

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const scrapeData = require('./scrapedata/scrape.js')
 const addToDatabase = require('./database/addToDatabase')
 const addToCsv = require('./toCsv/addToCsv')
 
-const gettingData = (usn, captcha, cookie)=>{
-    fetchData.fetchData(cookie,usn,captcha)
+const gettingData = (usn, captcha, cookie,token)=>{
+    fetchData.fetchData(cookie,usn,captcha,token)
         .then((data)=>{
             var obj = scrapeData.processdata(data,usn);
+            console.log(obj)
             if(obj === -1){
                 console.log("something went wrong")
             }
             else{
                 try{
-                    addToDatabase.addToDatabase(obj);
+                    console.log(obj)
+                    // addToDatabase.addToDatabase(obj);
                     // addToCsv.addToCsv(obj);
                 }
                 catch(err) {
@@ -28,12 +30,12 @@ const gettingData = (usn, captcha, cookie)=>{
         })
 }
 
-const getAllResults = (captcha, cookie)=> {
+const getAllResults = (captcha, cookie,token)=> {
     fetchUSN.usnArray()
         .then((usn)=>{
             for(var i = 0 ; i < usn.length ; i++){
                 var temp = usn[i][0];
-                gettingData(temp, captcha, cookie);
+                gettingData(temp, captcha, cookie,token);
             }
         })
         .catch((usn) => {
@@ -43,7 +45,7 @@ const getAllResults = (captcha, cookie)=> {
 
 const cookie = "urcaf3p3vjoaaisg8tclldkl01"; 
 var captcha = 93225;
-getAllResults(captcha, cookie)
+// getAllResults(captcha, cookie)
 
 
 module.exports = {
