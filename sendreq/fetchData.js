5c23f9b66e4b6dd016d62427c4e1160c3799176a
Zaid <zaidjan1295@gmail.com>
2019-03-16 16:22:00 +0530

    error handling, deleting csv file from server

--- sendreq/fetchData.js
+++ sendreq/fetchData.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const config = require('./config')
 const fetchData = (cookie,usn,captcha,token,year)=> {
     var obj = config.config(cookie, captcha, usn, token)
-    console.log("year", year)
-    console.log("http obj",obj[year].headers)
+    // console.log("year", year)
+    // console.log("http obj",obj[year].headers)
     var headers  = obj[year].headers;
     var myJSONObject = obj[year].myJSONObject;
+    var url = obj[year].url;
     return new Promise(function(resolve, reject) {
         request({
-            url: "http://results.vtu.ac.in/resultsvitavicbcs_19/resultpage.php",
+            // url: "http://results.vtu.ac.in/resultsvitavicbcs_19/resultpage.php",
+            url: url,
             method: "POST",
             headers: headers,
             formData :myJSONObject
